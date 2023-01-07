ef2d19022031eb768bf5d0f00507ecde6a13deee
Shane Dorsey <scdorsey22@gmail.com>
2023-01-07 17:08:25 -0500

    firebase configuration of entries

--- functions/firebase/functions/.eslintrc.js
+++ functions/firebase/functions/.eslintrc.js
@@ -1,14 +1,22 @@
 module.exports = {
-  root: true,
-  env: {
-    es6: true,
-    node: true,
-  },
-  extends: [
-    "eslint:recommended",
-    "google",
-  ],
-  rules: {
-    quotes: ["error", "double"],
-  },
-};
+    "env": {
+        "browser": true,
+        "es2021": true,
+        "node": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended"
+    ],
+    "overrides": [
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+    }
+}
--- functions/firebase/functions/index.js
+++ functions/firebase/functions/index.js
@@ -1,9 +1,13 @@
-const functions = require("firebase-functions");
+const functions = require('firebase-functions')
+const admin = require('firebase-admin')
+admin.initializeApp()
+const db = admin.firestore()
 
-// // Create and deploy your first functions
-// // https://firebase.google.com/docs/functions/get-started
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+exports.Entries = functions.https.onRequest(async (request, response) => {
+  const snapshot = await db.collection('entries').get()
+  const entries = snapshot.empty
+    ? []
+    : snapshot.docs.map(doc => Object.assign(doc.data(), { id: doc.id }))
+  response.send(entries)
+})
+
