1328ff06bd4ad6f62e4868cb16d7004895b0b131
Shane Dorsey <scdorsey22@gmail.com>
2023-03-12 17:01:45 -0700

    create

--- functions/firebase/functions/index.js
+++ functions/firebase/functions/index.js
@@ -3,11 +3,17 @@ const admin = require('firebase-admin')
 admin.initializeApp()
 const db = admin.firestore()
 
-exports.Entries = functions.https.onRequest(async (request, response) => {
-  const snapshot = await db.collection('entries').get()
+exports.entries = functions.https.onRequest(async (request, response) => {
+  const snapshot = await db.collection('Entries').get()
   const entries = snapshot.empty
     ? []
     : snapshot.docs.map(doc => Object.assign(doc.data(), { id: doc.id }))
   response.send(entries)
 })
 
+exports.createEntry = functions.https.onRequest(async (request, response) => {
+  const body = JSON.parse(request.body)
+  const text = body.text
+  const ref = await db.collection('Entries').add({ text })
+  response.send({ id: ref.id, text })
+})
